f79cc8126c59e0b29fa164aa92f461f570a11ee5
fersimorales <160299622+fersimorales@users.noreply.github.com>
2024-02-17 16:45:56 -0500

    Update script.js

--- script.js
+++ script.js
@@ -1,9 +1,7 @@
 var mapa = L.map('mapa').setView([-0.19,-78.50],12);
-L.tileLayer('https://tiles.stadiamaps.com/tiles/alidade_smooth_dark/{z}/{x}/{y}{r}.{ext}', {
-	minZoom: 0,
-	maxZoom: 20,
-	attribution: '&copy; <a href="https://www.stadiamaps.com/" target="_blank">Stadia Maps</a> &copy; <a href="https://openmaptiles.org/" target="_blank">OpenMapTiles</a> &copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors',
-	ext: 'png'
+L.tileLayer('https://tile.openstreetmap.org/{z}/{x}/{y}.png', {
+	maxZoom: 19,
+	attribution: '&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors'
 }).addTo(mapa);
 
 $.getJSON("https://fersimorales.github.io/PRUEBA1/archivo2.geojson",
